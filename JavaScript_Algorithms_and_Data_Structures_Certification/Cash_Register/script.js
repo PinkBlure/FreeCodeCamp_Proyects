132f096464c96478f0253a5feae89d52cbd164cd
Aileen <aileenpadrontorres511@gmail.com>
2024-01-13 22:03:17 +0000

    Cash register needs to implement get change

--- JavaScript_Algorithms_and_Data_Structures_Certification/Cash_Register/script.js
+++ JavaScript_Algorithms_and_Data_Structures_Certification/Cash_Register/script.js
@@ -1,4 +1,4 @@
-let price = 1.87;
+let price = 19.5;
 let cid = [
   ["PENNY", 1.01],
   ["NICKEL", 2.05],
@@ -22,7 +22,46 @@ purchase.addEventListener('click', ()=>{
   console.log(cash.value, price)
   if (cash.value < price) {
     alert('Customer does not have enough money to purchase the item')
+    return
   } else if (parseFloat(cash.value) === price) { 
     change.innerText = 'No change due - customer paid with exact cash'
+    return
   }
+
+  const changeTotal = parseFloat(cash.value) - price
+  console.log(changeTotal)
+  checkFund(changeTotal)
+
 })
+
+const checkFund = (changeCoin) => {
+  let cashInDrawer = 0
+  cid.forEach((coin) => {
+    cashInDrawer += coin[1]
+  })
+  cashInDrawer = parseFloat(cashInDrawer.toFixed(2))
+  if (cashInDrawer < changeCoin) {
+    change.innerText = 'Status: INSUFFICIENT_FUNDS'
+    return
+  } else if (cashInDrawer === changeCoin) {
+    change.innerText = 'Status: CLOSED'
+    return
+  }
+}
+
+const getChange = (changeCoin) => {
+  let changeTotal = changeCoin
+  switch (true) {
+    case changeTotal >= 100:
+    case changeTotal >= 20:
+    case changeTotal >= 10:
+    case changeTotal >= 5:
+    case changeTotal >= 1:
+    case changeTotal >= 0.25:
+    case changeTotal >= 0.1:
+    case changeTotal >= 0.05:
+    case changeTotal >= 0.01:
+    case changeTotal === 0:
+    default:
+  }
+} 
