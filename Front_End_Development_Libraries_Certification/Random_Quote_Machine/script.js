5bfc8c46f7a350a4d3fa23fdbbd50c44d73c7d77
PinkBlure <aileenpadrontorres511@gmail.com>
2024-01-25 09:26:45 +0000

    Added card by react component. Add a quote icon with an i tag on react

---
+++ Front_End_Development_Libraries_Certification/Random_Quote_Machine/script.js
@@ -0,0 +1,17 @@
+function App() {
+    return (
+      <div className='card-body'>
+        <blockquote>
+          <p id='text' className='card-text'><i className='fas fa-quote-left'/>La cita va aquí</p>
+          <cite id='author'>Nombre del Autor</cite>
+        </blockquote>
+        <div>
+          <a id='tweet-quote' className='card-link' href='https://twitter.com/intent/tweet' target='_blank'>Twitter</a>
+          <a id='tumbl-quote' className='card-link' target='_blank'>Tumbl</a>
+        </div>
+        <button id='new-quote'>New quote</button>
+      </div>
+    );
+  }
+  
+  ReactDOM.render(<App />, document.getElementById('quote-box'));
