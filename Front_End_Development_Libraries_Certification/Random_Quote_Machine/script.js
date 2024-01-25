c9981baa5f18f8b0748e5e9e905d6ac55b69191f
PinkBlure <aileenpadrontorres511@gmail.com>
2024-01-25 13:23:26 +0000

    Fetch with async await working when new quote clicked

--- Front_End_Development_Libraries_Certification/Random_Quote_Machine/script.js
+++ Front_End_Development_Libraries_Certification/Random_Quote_Machine/script.js
@@ -1,17 +1,42 @@
-function App() {
-    return (
-      <div className='card-body'>
-        <blockquote>
-          <p id='text' className='card-text'><i className='fas fa-quote-left'/>La cita va aquí</p>
-          <cite id='author'>Nombre del Autor</cite>
-        </blockquote>
-        <div>
-          <a id='tweet-quote' className='card-link' href='https://twitter.com/intent/tweet' target='_blank'>Twitter</a>
-          <a id='tumbl-quote' className='card-link' href = '#' target='_blank'>Tumbl</a>
-        </div>
-        <button id='new-quote'>New quote</button>
-      </div>
-    );
+class Quote extends React.Component {
+    
+  constructor(props) {
+    super(props)
   }
   
-  ReactDOM.render(<App />, document.getElementById('quote-box'));
+  render() {
+    return (
+      <div className='card-body'>
+        
+        <blockquote className='text-end'>
+          <p id='text' className='card-text text-center fs-2'>{/* <i className='fas fa-quote-left'/> */}Loading . . .</p>
+          <cite id='author'></cite>
+        </blockquote>
+        
+        <div>
+          <a id='tweet-quote' className='card-link' href='https://twitter.com/intent/tweet' target='_blank'><i className='fab fa-twitter'/></a>
+          <a id='tumbl-quote' className='card-link' href = '#' target='_blank'><i className='fab fa-tumblr'/></a>
+        </div>
+        
+        <button id='new-quote' style={{ fontSize: '0.8rem' }} onClick={updateQuote}>New quote</button>
+        
+      </div>
+    )
+  }
+}
+
+// Fetch a random quote from the Quotable API
+async function updateQuote() {
+
+  const response = await fetch("https://api.quotable.io/random")
+  const data = await response.json()
+  
+  if (response.ok) {
+    console.log(data.content)
+    console.log(data.author)
+  } else {
+    console.log("An error occured")
+  }
+}
+
+ReactDOM.render(<Quote />, document.getElementById('quote-box'))
