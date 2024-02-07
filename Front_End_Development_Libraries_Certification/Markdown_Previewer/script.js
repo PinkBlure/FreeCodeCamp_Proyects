b4de60a2cb389f4b6af93f3d049097c3962fd8df
PinkBlure <aileenpadrontorres511@gmail.com>
2024-02-07 08:41:22 +0000

    Fixed distribution of components

--- Front_End_Development_Libraries_Certification/Markdown_Previewer/script.js
+++ Front_End_Development_Libraries_Certification/Markdown_Previewer/script.js
@@ -13,9 +13,12 @@ class Editor extends React.Component {
 
   render() {
     return (
-      <div className='col p-0 border'>
+      <div className='p-0 border'>
         <div className='card-header d-flex justify-content-between align-items-center'>
-          <span>Text Editor</span>
+          <span>
+            <i className="bi bi-emoji-smile"></i>
+            {' '}Editor
+          </span>
           <button type="button" onClick={this.toggleCollapse}>
             <i className={this.state.collapsed ? "fas fa-window-maximize" : "fas fa-window-minimize"}></i>
           </button>
@@ -32,23 +35,50 @@ class Editor extends React.Component {
 }
 
 
-// class Previewer extends React.Component {
-//   constructor(props) {
-//     super(props)
-//   }
-//   render() {
-//     return (
-//       <div className='col card'>
-//         <div className='card-header'>Text Editor</div>
-//         <div className='card-body'>Esto es un editor</div>
-//       </div>
-//     )
-//   }
-// }
+class Previewer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapsed: false
+    };
+    this.toggleCollapse = this.toggleCollapse.bind(this);
+  }
+
+  toggleCollapse() {
+    this.setState({ collapsed: !this.state.collapsed });
+  }
+
+  render() {
+    return (
+      <div className='p-0 border'>
+        <div className='card-header d-flex justify-content-between align-items-center'>
+          <span>
+            <i className="bi bi-emoji-smile-fill"></i>
+            {' '}Previewer
+          </span>
+          <button type="button" onClick={this.toggleCollapse}>
+            <i className={this.state.collapsed ? "fas fa-window-maximize" : "fas fa-window-minimize"}></i>
+          </button>
+        </div>
+        <div 
+          id='editor-card-body' 
+          className={`card-body ${this.state.collapsed ? 'collapse' : ''}`} 
+          style={{ height: this.state.collapsed ? '0' : 'auto', overflow: 'hidden' }}>
+          <textarea id='editor' className='w-100'></textarea>
+        </div>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <div class='row'>
-    <Editor />
+  <div className='row mt-3 mx-3'>
+    <div className='col-6'>
+      <Editor />
+    </div>
+    <div className='col-6'>
+      <Previewer />
+    </div>
   </div>,
   document.getElementById('root')
 )
